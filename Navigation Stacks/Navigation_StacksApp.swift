fd70cc11be8f7c85cca3c7f2ff768f17d3272b4b
rangerboy574 <jdeweese574@gmail.com>
2023-01-30 11:18:59 -0600

    Initial Commit

---
+++ Navigation Stacks/Navigation_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_StacksApp.swift
+//  Navigation Stacks
+//
+//  Created by Joseph  DeWeese on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
